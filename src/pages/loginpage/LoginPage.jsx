7a0ea2a3cdbc229d07c68c947c1fb88af0989765
r4ppz <johnreyrabosa.f@gmail.com>
2025-09-07 11:00:13 +0800

    style: enhance Login UI with Logo and Description

--- src/pages/loginpage/LoginPage.jsx
+++ src/pages/loginpage/LoginPage.jsx
@@ -1,12 +1,18 @@
 import style from "./LoginPage.module.css";
 import Form from "../../components/form/Form";
+import SchoolLogo from "../../assets/school-logo.svg";
 
 function LoginPage() {
   return (
     <div className={style.page}>
       <div className={style.mainCard}>
         <div className={style.introCard}>
+          <img className={style.logo} src={SchoolLogo} alt="acd-logo" />
           <h1 className={style.title}>Assumption Research Library</h1>
+          <h3 className={style.description}>
+            A digital library of research and innovations from our faculty and
+            students.
+          </h3>
         </div>
         <div className={style.formCard}>
           <h2 className={style.header}>Admin login</h2>
