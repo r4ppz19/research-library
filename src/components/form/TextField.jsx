61eab99dbbb1dff4de71df5bb9d4f7e5fb5ae83b
r4ppz <johnreyrabosa.f@gmail.com>
2025-09-08 18:16:25 +0800

    refractor: update and add new varibles

--- src/components/form/TextField.jsx
+++ src/components/form/TextField.jsx
@@ -2,7 +2,6 @@ import style from "./TextField.module.css";
 
 function TextField({
   id,
-  label,
   value,
   onChange,
   placeholder,
@@ -10,32 +9,20 @@ function TextField({
   padding,
   type = "text",
 }) {
-  const showIconInLabel = label && icon;
-  const showIconInInput = !label && icon;
-
   return (
-    <div className={style.inputGroup}>
-      {label && (
-        <label className={style.label} htmlFor={id}>
-          {showIconInLabel && <span className={style.icon}>{icon}</span>}
-          {label}
-        </label>
-      )}
-
-      <div
-        className={style.inputWrapper}
-        style={padding ? { padding } : undefined}
-      >
-        {showIconInInput && <span className={style.icon}>{icon}</span>}
-        <input
-          className={style.input}
-          id={id}
-          type={type}
-          value={value}
-          onChange={onChange}
-          placeholder={placeholder}
-        />
-      </div>
+    <div
+      className={style.inputWrapper}
+      style={padding ? { padding } : undefined}
+    >
+      {icon && <span className={style.icon}>{icon}</span>}
+      <input
+        className={style.input}
+        id={id}
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
     </div>
   );
 }
